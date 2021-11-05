656c6bbcf21caac413176c3b89da3b92dc627713
ashleigh-tesla <84648968+ashleigh-tesla@users.noreply.github.com>
2021-11-05 10:00:34 +0200

    first commit

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,32 +1,126 @@
 <template>
-  <div>
-    Hello World 
+  <div
+    class="p-3"
+    style="max-width: 400px; margin: 50px auto; background: #234"
+  >
+    <!-- Calculator Result -->
+    <div
+      class="
+        w-full
+        rounded
+        m-1
+        p-3
+        text-right
+        lead
+        font-weight-bold
+        text-white
+        bg-vue-dark
+      "
+    >
+      {{ calculatorValue || 0 }}
+    </div>
+    <!-- Calculator Buttons -->
+    <div class="row no-gutters">
+      <div class="col-3" v-for="n in calculatorElements" :key="n">
+        <div
+          class="
+            lead
+            text-white text-center
+            m-1
+            py-3
+            bg-vue-dark
+            rounded
+            hover-class
+          "
+          :class="{
+            'bg-vue-green': ['C', '*', '/', '-', '+', '%', '='].includes(n),
+          }"
+          @click="action(n)"
+        >
+          {{ n }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Calculator',
+  name: "Calculator",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      calculatorValue: "",
+      calculatorElements: [
+        "C",
+        "*",
+        "/",
+        "-",
+        7,
+        8,
+        9,
+        "+",
+        4,
+        5,
+        6,
+        "%",
+        1,
+        2,
+        3,
+        "=",
+        0,
+        ".",
+      ],
+      operator: null,
+      previousCalculatorValue: "",
+    };
+  },
+
+  methods: {
+    action(n) {
+      // Append Value
+      if (!isNaN(n) || n === ".") {
+        this.calculatorValue +=  n + "";
+      }
+      // Clear Value
+      if (n === "C") {
+        this.calculatorValue = "";
+      }
+      // Percentage
+      if (n === "%") {
+        this.calculatorValue = this.calculatorValue / 100 + "";
+      }
+      // Arithmetics
+      if (["/", "*", "/", "-", "+"].includes(n)) {
+        this.operator = n;
+        this.previousCalculatorValue = this.calculatorValue;
+        this.calculatorValue = "";
+      }
+      if (n === "=") {
+        this.calculatorValue = eval(
+          this.previousCalculatorValue + this.operator + this.calculatorValue
+        );
+
+        this.previousCalculatorValue = "";
+        this.operator = null;
+      }
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* h3 {
-  margin: 40px 0 0;
+.bg-vue-dark {
+  background: #31475e;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.hover-class:hover {
+  background: #3d5875;
+  cursor: pointer;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.bg-vue-green {
+  background: #3fb984;
 }
-a {
-  color: #42b983;
-} */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'jquery/src/jquery.js';
+import 'popper.js/dist/popper.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
